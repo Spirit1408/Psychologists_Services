00726765b4ff6187adb8cf4cdb375d071a8caf6b
Oleh Shalashov <spirit14081992@gmail.com>
2025-03-15 09:31:48 -0300

    Update minor fix

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBc2n7s8OnGN9eSxbNPuFtnq0Zre8yX7ec",
+    authDomain: "psychologists-services-bcea3.firebaseapp.com",
+    databaseURL: "https://psychologists-services-bcea3-default-rtdb.firebaseio.com",
+    projectId: "psychologists-services-bcea3",
+    storageBucket: "psychologists-services-bcea3.firebasestorage.app",
+    messagingSenderId: "624506661953",
+    appId: "1:624506661953:web:88069570d82efe9f0e2ac0",
+  };
